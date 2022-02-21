1475d71a0cc882269a5cb09fec5fc31653be4a63
nickecon <72775861+nickecon@users.noreply.github.com>
2022-02-21 23:45:29 +0000

    Mon, Feb 21, 2022, 11:45 PM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -49,7 +49,7 @@ int get_cents(void)
 int calculate_quarters(int cents)
 {
     // TODO
-    return cents = cents -  * 25;
+    return cents = cents - quarters * 25;
 }
 
 int calculate_dimes(int cents)
