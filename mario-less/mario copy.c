61fb05912e5ab2ad0f7d70dac69b89a9a66d89da
nickecon <72775861+nickecon@users.noreply.github.com>
2022-02-21 21:37:26 +0000

    Mon, Feb 21, 2022, 9:37 PM +00:00

---
+++ mario-less/mario copy.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 0 || n > 8);
+
+    //for each row
+    for (int i = 0; i < n; i++)
+    {
+        //for each column
+        for (int j = 0; j < i+1; j++)
+        {
+            printf("#");
+        }
+        //print new line
+        printf("\n");
+    }
+}
