045a1eb4a36e4ff87ffb7a9422e82eef076b1e96
nickecon <72775861+nickecon@users.noreply.github.com>
2022-02-21 23:25:13 +0000

    Mon, Feb 21, 2022, 11:25 PM +00:00

--- cash/cash.c
+++ cash/cash.c
@@ -10,7 +10,12 @@ int calculate_pennies(int cents);
 int main(void)
 {
     // Ask how many cents the customer is owed
-    int cents = get_cents("Change Amount?");
+    int cents = get_cents();
+    do
+    {
+        cents = get_int("Change Amount? ");
+    }
+    while (cents < 0 || cents > 99);
 
     // Calculate the number of quarters to give the customer
     int quarters = calculate_quarters(cents);
