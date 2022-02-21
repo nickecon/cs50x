80c5bd8c650a664e061b9babdca3a8dc091d7400
nickecon <72775861+nickecon@users.noreply.github.com>
2022-02-21 21:19:34 +0000

    Mon, Feb 21, 2022, 9:19 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -14,7 +14,7 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         //for each column
-        for (int j = 0; j < i+1; j++)
+        for (int j = 0; j < i; j++)
         {
             printf("#");
         }
