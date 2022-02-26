7a9289b49fca3408657d710898046aa985e18873
nickecon <72775861+nickecon@users.noreply.github.com>
2022-02-26 06:51:06 +0000

    Sat, Feb 26, 2022, 6:51 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -17,18 +17,16 @@ int main(void)
     {
         printf("In Range\n");
     }
-    int mod = 0;
+    int mod = 10;
     int tot = 0;
-    for (int i = i; int j = 11; i+=2) {
-        while (i <= j) {
-            mod = pow(10, i)
-            int temp = n%mod
-            int temp2 = n%(mod + 1)
-            temp = (temp2 - temp)/temp
-            printf("d", tot)
-            tot = tot + (temp*2)
-            printf("d", tot)
-        };
+    for (int i = i; i < 11; i+=2) {
+            int temp = n%mod;
+            mod = mod * 10;
+            int temp2 = n%mod;
+            temp = (temp2 - temp)/temp;
+            printf("d", tot);
+            tot = tot + (temp*2);
+            printf("d", tot);
     }
     else
     {
