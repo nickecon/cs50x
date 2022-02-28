958981413b69114439087246d1d7e61c26365c51
nickecon <72775861+nickecon@users.noreply.github.com>
2022-02-28 09:13:46 +0000

    Mon, Feb 28, 2022, 9:13 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,6 +3,17 @@
 
 bool only_digits(string s);
 
+//make sure program was run w just one command line argument
+
+//make sure every character in argv[1] is a digit
+
+//convert avg[1] from a string to an int
+
+//prompt user for plaintext
+
+//for each character in the plaintext:
+
+    //rotate the character if its a letter
 int main(int argc, string argv[])
 {
     if (argc==1 || argc>=4)
@@ -16,7 +27,7 @@ int main(int argc, string argv[])
     }
     if (argc==3)
     {
-        
+
         printf("ciphertext: %s\n", argv[0]);
         return 0;
     }
