40e5b7ae423da7de3258a41e65fd069ad9af8106
nickecon <72775861+nickecon@users.noreply.github.com>
2022-02-28 20:15:46 +0000

    Mon, Feb 28, 2022, 8:15 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -19,13 +19,12 @@ int main(int argc, string argv[])
 {
     if (argc>=4)
     {
-
         printf("Usage: ./caesar key\n");
         return 0;
     }
     if (argc==1)
     {
-
+        return 1;
     }
     if (argc==2)
     {
@@ -33,16 +32,16 @@ int main(int argc, string argv[])
     }
     if (argc==3)
     {
-        if
-        {
-            (only_digits(argv[3]));
-        }
-        else
-        {
+       // if
+       // {
+       //     (only_digits(argv[3]));
+       // }
+       // else
+       // {
             printf("ciphertext: %s\n", argv[2]);
-        }
+    }
         return 0;
     }
-}
+
 
 bool only_digits(string s);
