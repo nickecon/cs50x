a611cd213cccc53b9b007c7b464b50a989cf6fce
nickecon <72775861+nickecon@users.noreply.github.com>
2022-02-28 09:14:59 +0000

    Mon, Feb 28, 2022, 9:14 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -19,11 +19,11 @@ int main(int argc, string argv[])
     if (argc==1 || argc>=4)
     {
         printf("Usage: ./caesar key\n");
-        return 0;
+        return 1;
     }
     if (argc==2)
     {
-        return 1;
+        return 0;
     }
     if (argc==3)
     {
