5d1b11cfbcd48afd768fb553f30f4f22645c67b8
nickecon <72775861+nickecon@users.noreply.github.com>
2022-02-28 21:33:26 +0000

    Mon, Feb 28, 2022, 9:33 PM +00:00

---
+++ caesar/caesar2.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+
+//bool only_digits(string s);
+//int isdigit(char c);
+//make sure program was run w just one command line argument
+
+//make sure every character in argv[1] is a digit
+
+//convert avg[1] from a string to an int
+
+//prompt user for plaintext
+
+//for each character in the plaintext:
+
+    //rotate the character if its a letter
+int main(int argc, string argv[])
+{
+    if (argc>=3 || argc==1)
+    {
+        printf("Usage: ./caesar key\n");
+        return 01;
+    }
+    if (argc==2)
+    {
+        p
+    }
+}
