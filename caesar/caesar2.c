51e4420df8f7148b90325041f827cb5ead4a5f1d
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-01 03:38:22 +0000

    Tue, Mar 1, 2022, 3:38 AM +00:00

--- caesar/caesar2.c
+++ caesar/caesar2.c
@@ -34,7 +34,7 @@ int main(int argc, string argv[])
 
         for (int i = 0, n = strlen(s); i < n; i++)
         {
-            printf("%c", rotate(s[i]), k);
+            printf("%c", rotate(s[n]), k);
             //printf("%c", rotate(s[i]), k);
         }
 
