3f10213f639668880821974f024596e82335b5cf
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-01 04:52:02 +0000

    Tue, Mar 1, 2022, 4:52 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,8 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
 bool only_digits(string s);
+char rotate(char c, int k);
 //int isdigit(char c);
 //make sure program was run w just one command line argument
 
@@ -20,41 +23,68 @@ int main(int argc, string argv[])
     if (argc>=3 || argc==1)
     {
         printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    else if (argc==2 && only_digits(argv[1]))
+    {
+        string s = get_string("plaintext:  ");
+        int k = atoi(argv[1]);
+        //char c[strlen(s) +1];
+        printf("ciphertext: ");
+
+        for (int i = 0, n = strlen(s); i < n; i++)
+        {
+            char cipher = rotate(s[i], k);
+            printf("%c", cipher);
+            //printf("%c", rotate(s[i]), k);
+        }
+
+        //(int i = 0, n = strlen(s); i < n; i++)
+        //{
+        //    printf("%c", s[i]);
+        //}
+        printf("\n");
         return 0;
     }
-    if (argc==2)
+
+    else
     {
-        if (argc==2)
-        {
-            only_digits()
-            get_string("ciphertext: %s\n", argv[1]);
-            return 0;
-        }
-        else
-        {
-            printf("Usage: ./caesar key\n");
+        printf("Usage: ./caesar key\n");
         return 1;
-        }
     }
-
-
-
-
-    //if (argc==3)
-    //{
-       // if
-       // {
-       //     (only_digits(argv[3]));
-       // }
-       // else
-       // {
-     //       printf("ciphertext: %s\n", argv[2]);
-    //}
-     //   return 0;
 }
 
 
 bool only_digits(string s)
 {
+    for (int i = 0; i < strlen(s); i++)
+        {
+            if (isdigit(s[i]))
+            {
+                return true;
+            }
+            else
+            return false;
+        }
+    return s;
+}
 
+char rotate(char c, int k)
+{
+    if (isalpha(c))
+    {
+        if (isupper(c))
+        {
+            return c = ((c-65)+k)%26 + 65;
+        }
+        else
+        {
+            return c = ((c-97)+k)%26 + 97;
+        }
+    }
+    else
+    {
+        return c;
+    }
 }
