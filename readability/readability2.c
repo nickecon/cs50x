310535b076b7e303f897d49cef626f7155176809
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-02 06:35:07 +0000

    Wed, Mar 2, 2022, 6:35 AM +00:00

---
+++ readability/readability2.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int count_letters(string s);
+
+int main(void)
+{
+string s = get_string("Text: ");
+int input = count_letters(s);
+printf("%i letters", input);
+printf("\n");
+//printf("%s\n", s);
+}
+
+int count_letters(string s)
+{
+    int count = 0;
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (isalpha(s[i]))
+        {
+            return count++;
+        }
+    }
+    return count;
+}
