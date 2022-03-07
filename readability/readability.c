a8b6857460f47ed3e6422b71472a1738875816f3
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-07 23:33:01 +0000

    Mon, Mar 7, 2022, 11:33 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -6,15 +6,30 @@
 
 int count_letters(string s);
 int count_words(string s);
-int count_sentances
+int count_sentences(string s);
 
 int main(void)
 {
     string s = get_string("Text: ");
     int letters = count_letters(s);
     int words = count_words(s);
+    int sentences = count_sentences(s);
     printf("%i letters\n", letters);
     printf("%i words\n", words);
+    printf("%i sentences\n", sentences);
+}
+
+int count_sentences(string s)
+{
+    int i = 0;
+    for (int j = 0; j < strlen(s); j++)
+    {
+        if (s[j] == '.')
+        {
+            i++;
+        }
+    }
+    return i;
 }
 
 int count_words(string s)
