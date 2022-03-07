f86f6b0f85e860e052d693058b6c6ba9d284c961
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-07 21:33:59 +0000

    Mon, Mar 7, 2022, 9:33 PM +00:00

---
+++ readability/readability4.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int count_letters(string s);
+
+int main(void)
+{
+    string s = get_string("Text: ");
+    int l = count_letters(s);
+    printf("%i", l);
+    printf(" letters\n");
+}
+
+int count_letters(string s)
+{
+    int i = 0;
+    for (int j = 0; j < strlen(s); j++)
+    {
+        while (isalpha(s[i]))
+        {
+            i++;
+        }
+    }
+    return i;
+}
+
+    //for (int j = 0, n = strlen(s); j < n; j++)
+    //{
