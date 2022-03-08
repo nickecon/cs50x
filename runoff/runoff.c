d57fa962cf3349b97776efe1ecd106105d205b48
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-08 23:18:52 +0000

    Tue, Mar 8, 2022, 11:18 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,11 +127,12 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for (int i = 0,j =0; i < candidate_count; i++,j++)
+    for (int i = 0; i < candidate_count; i++)
     {
+        if (strcmp rank, candid)
         if (strcmp(name, candidates[i].name)) == 0)
         {
-            candidates[i][j].votes++;
+            candidates[i].votes+3;
             return true;
         }
     }
