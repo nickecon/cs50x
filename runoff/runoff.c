644e6ee87dd4051db5b6b9e1ee54122c7f269b5f
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-08 22:55:37 +0000

    Tue, Mar 8, 2022, 10:55 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -129,9 +129,9 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidate[i].name)) == 0)
+        if (strcmp(name, candidates[i].name)) == 0)
         {
-            candidates[]
+            candidates[i]
         }
     }
     return false;
