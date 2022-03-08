bf627356157e3c0919db2a59f887ef14ea6023f6
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-08 04:46:26 +0000

    Tue, Mar 8, 2022, 4:46 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,13 +80,13 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    for (int i = 0,j = 0; i < candidate_count; i++)
+    for (int i = 0,j = 0; i < candidate_count; i++, j++)
     {
         if (candidates[i].votes < candidates[j].votes)
         {
             printf("%s\n", candidates[j].name);
         }
-        else (candidates[i].votes > candidates[j].votes)
+        else if (candidates[i].votes > candidates[j].votes)
         {
             printf("%s\n", candidates[i].name);
         }
