18f3940787a511f98688f93af55f46698b579433
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-09 04:18:50 +0000

    Wed, Mar 9, 2022, 4:18 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -147,7 +147,7 @@ void tabulate(void)
         {
             for (int j = 0; j < candidate_count; j++)
             {
-                if preferences[i][j] 
+                if (candidates[preferences[i][j]])
             }
         }
     return;
