a7f4abc3c31d1f6c785ec5d15793404e0cfbff4e
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-13 19:45:30 +0000

    Sun, Mar 13, 2022, 7:45 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -189,11 +189,7 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    for (int i = 0; i < candidate_count; i++)
-    {
-
-        if (candidates[i].votes == min && !candidates[i].eliminated)
-        min = candidates[i].votes;
+    while (candidates[i].votes == min && !candidates[i].eliminated)
         return true;
     }
     return false;
