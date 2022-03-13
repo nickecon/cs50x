78957522b3de4af5939ff69af92bbea789fa69ee
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-13 18:49:09 +0000

    Sun, Mar 13, 2022, 6:49 PM +00:00

--- runoff/runoff3.c
+++ runoff/runoff3.c
@@ -158,7 +158,7 @@ bool print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (!candidates[i].eliminated)
+        if (candidates[i].votes > voter_count/2)
         {
             printf("%s\n", candidates[i].name);
             return true;
