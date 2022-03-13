d38fe9c4429e1aa3802b7b3e525d9897fd190ccd
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-13 19:59:50 +0000

    Sun, Mar 13, 2022, 7:59 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -191,12 +191,12 @@ bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes == min && !candidates[i].eliminated)
+        if (candidates[i].votes > min && !candidates[i].eliminated)
         {
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
