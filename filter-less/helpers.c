1d370b7930e198afe96ac704e78d38166b46704a
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-16 03:37:37 +0000

    Wed, Mar 16, 2022, 3:37 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            ((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            image[i][j].rgbtRed = ((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3.0);
         }
     }
     return;
