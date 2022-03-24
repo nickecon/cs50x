74becf6b6608305ad8d76cea8fcd589cdaf68280
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-24 05:49:32 +0000

    Thu, Mar 24, 2022, 5:49 AM +00:00

--- filter-less/helpers2.c
+++ filter-less/helpers2.c
@@ -61,6 +61,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 
         }
     }
+    if (image[0][0] || image[0][width-1]) || image[height-1][0] || image[height-1][width-1])
     image[0][0] = image[i-1][j] + image[i-1][j+1] + image[i][j+1] + image[i+1][j+1] + image[i+1][j] + image[i+1][j-1] + image[i][j-1] + image[i-1][j-1] + image[i][j] / 4;
     if // surrounding is edge
     int blur = image[i-1][j] + image[i-1][j+1] + image[i][j+1] + image[i+1][j+1] + image[i+1][j] + image[i+1][j-1] + image[i][j-1] + image[i-1][j-1] + image[i][j] / 6;
