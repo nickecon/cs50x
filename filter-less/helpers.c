3b5944df33829139084878c3c68dcb058ff3d4b0
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-24 06:29:56 +0000

    Thu, Mar 24, 2022, 6:29 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -64,7 +64,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width/2; j++)
+        for (int j = 0; j < width / 2; j++)
         {
             RGBTRIPLE tmp;
             tmp = image[i][j];
