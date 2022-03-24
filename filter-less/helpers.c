e793b1878d0b44dfab91d747bdf2ca66b79ce52f
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-24 04:41:09 +0000

    Thu, Mar 24, 2022, 4:41 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -79,5 +79,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width/2; j++)
+        {
+            a
+        }
     return;
 }
