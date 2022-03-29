8951b7b841c7c3e3e7109d3604d1be850bf9d061
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-29 02:53:11 +0000

    Tue, Mar 29, 2022, 2:53 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
         int arr[512]
         while fread(arr, 1, 512, file) == 512
         {
-            a
+            if 
         }
 
     //start of JPEG -- 0xff 0xd8 0xff 0xe0-0xef
