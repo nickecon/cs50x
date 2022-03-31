18c856976e9e1230fe48947e7424c5707c1fda69
nickecon <72775861+nickecon@users.noreply.github.com>
2022-03-31 01:42:34 +0000

    Thu, Mar 31, 2022, 1:42 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
     BYTE arr[512];
 
     //create filename
-    char *filename = malloc(3*sizeof(int));
+    char *filename = malloc(3 * sizeof(int));
 
     //create jpeg counter
     int count = 0;
@@ -54,47 +54,3 @@ int main(int argc, char *argv[])
     return 0;
     fclose(input);
     free(arr);
-
-//1) Search for header
-//    a) Header found - close output file if it exists
-//   b) Generate new filename
-//   c) Open new file
-//   d) Write to file
-//2) Header not found - If output file is open, write to file.
-
-    //start of JPEG -- 0xff 0xd8 0xff 0xe0-0xef
-    //arr[0] == 0xff;
-    //arr[1] == 0xd8;
-    //arr[2] == 0xff;
-    //(arr[3] & 0xf0) == 0xe0;
-
-    //making a new JPEG
-    //sprintf(filename, "%03i.jpg", 1);
-    //FILE * img = fopen(filename, "w");
-
-
-
-    //fread(data, size, number, inptr);
-    //data: pointer to where to store data you're reading
-    //size: size of each element to read
-    //number: number of elements to read
-    //inptr: FILE * to read from
-
-    //fwrite(date, size, number, outptr);
-    //data: pointer to bytes that will be written to file
-    //size: size of each element to write
-    //number: number of elements to wrtie
-    //outptr: FILE * to write to
-    //Returns number of items of size size were read
-}
-
-//Open memory card
-//Repeat until end of card:
-//  Read 512 bytes into a buffer - allocate room into memory
-//  If start of new JPEG - read those 512 bytes that were allocated and see if start of new JPEG
-//      If first JPEG 001.jpg - start writing first file
-//
-//      Else - close to open another
-//  Else
-//      If already found JPEG - keep writing another 512 bytes
-//Close any remaining files
