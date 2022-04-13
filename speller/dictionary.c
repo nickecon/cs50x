a1dcc33a18663f9c4515861bed6bcfed0c652986
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-13 03:37:30 +0000

    Wed, Apr 13, 2022, 3:37 AM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cs50.h>
 
 #include "dictionary.h"
 
@@ -33,7 +34,7 @@ bool check(const char *word)
     int value = hash(c);
     for (int i = 0; i < value, i++)
     {
-        
+        if strcasecmp(c, )
         table[i]
     }
     //{
