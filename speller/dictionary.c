96ea9d02eb9f938d0ab62fb0d110eb6920a11ed7
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-14 22:36:21 +0000

    Thu, Apr 14, 2022, 10:36 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -2,11 +2,10 @@
 
 #include <ctype.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <strings.h>
-
 #include "dictionary.h"
 
 // Represents a node in a hash table
