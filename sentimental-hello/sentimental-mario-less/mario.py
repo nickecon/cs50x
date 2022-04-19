0108ea354bff4264086721b01f418c0fc0a31243
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-19 06:28:57 +0000

    Tue, Apr 19, 2022, 6:28 AM +00:00

--- sentimental-hello/sentimental-mario-less/mario.py
+++ sentimental-hello/sentimental-mario-less/mario.py
@@ -1,10 +1,12 @@
 from cs50 import get_int
 
+# ask for height and get numbers 1-8
 while True:
     n = get_int("Height: ")
     if n > 0 and n < 9:
         break
 
+# run a loop to get the needed bricks
 count = 1
 for i in range(n):
     print(' ' * (n-1) + "#"*count)
