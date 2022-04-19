af20558a17d0abb63446f7788a75f5a0965319f5
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-19 06:53:49 +0000

    Tue, Apr 19, 2022, 6:53 AM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -3,12 +3,12 @@ from cs50 import get_int
 # ask for change owed
 while True:
     change = get_int("Change owed: ")
-    if change > 0:
+    if change > 0.0:
         break
 
 qu = change / 25
 di = (change - (qu * 25)) / 10
 ni = (change - (qu * 25) - (di * 10)) / 5
-pe = (change - (qu * 25) - (di * 10) - (ni * 5)
+pe = (change - (qu * 25) - (di * 10) - (ni * 5))
 coins = qu + di + ni + pe
 print(coins)
