613b1d56deff9c31518740a6a360a5104c092053
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-19 06:22:01 +0000

    Tue, Apr 19, 2022, 6:22 AM +00:00

--- sentimental-hello/sentimental-mario-less/mario.py
+++ sentimental-hello/sentimental-mario-less/mario.py
@@ -5,14 +5,8 @@ while True:
     if n > 0 and n < 9:
         break
 
-#count = 1
-#for i in range(n):
-#    print(' ' * (n-1),"#"*count)
-#    count += 1
-#    n -= 1
-
+count = 1
 for i in range(n):
-    for j in range(n):
-        count =1
-        print(' ' * (n-1), "#" * count)
-        count += 1
+    print(' ' * (n-1),"#"*count)
+    count += 1
+    n -= 1
