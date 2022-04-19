759ca85dbb5b1fed49f45098bdb8785655f2f006
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-19 05:46:58 +0000

    Tue, Apr 19, 2022, 5:46 AM +00:00

--- sentimental-hello/sentimental-mario-less/mario.py
+++ sentimental-hello/sentimental-mario-less/mario.py
@@ -6,7 +6,7 @@ while True:
         break
 
 for i in range(n):
-    for j in range(n, -1):
+    for j in range(n-1):
         print(" ", end="")
     for k in range(n-1, n):
         print("#" * n)
