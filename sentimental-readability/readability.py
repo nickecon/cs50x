577de3189159d5f641390d4152a8459356d172b6
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-20 06:13:03 +0000

    Wed, Apr 20, 2022, 6:13 AM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -22,9 +22,9 @@ S = (se/w * 100)
 index = round(0.0588 * L - 0.296 * S - 15.8)
 
 # print proper Grade
-if index < 1:
-    print("Before Grade 1")
-elif index > 16:
+if index > 16:
     print("Grade 16+")
+elif index < 1:
+    print("Before Grade 1")
 else:
     print(f"Grade {index}")
