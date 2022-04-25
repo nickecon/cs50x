2653554195bdac6a6515a5d36a489aea4415c630
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-25 21:14:54 +0000

    Mon, Apr 25, 2022, 9:14 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -46,7 +46,7 @@ def main():
     # TODO: Check database for matching profiles
     for row in reader:
         for i in range(len(STR)):
-            if int(row[STR[i]]) == matches[row[STR[i]]]:
+            if int(row[STR[i]]) == matches[STR[i]]:
                 match = row["name"]
                 print(match)
             break
