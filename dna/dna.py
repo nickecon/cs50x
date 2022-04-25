143377632c8010be07a694518eb07267adf9abf1
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-25 23:04:50 +0000

    Mon, Apr 25, 2022, 11:04 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -46,12 +46,10 @@ def main():
     for row in reader:
         for i in range(len(STR)):
             if int(row[STR[i]]) == matches[STR[i]]:
-                match = row["name"]
-                print(match)
+                bool = True
             else:
-                print("No Match")
-                break
-            break
+                bool = False
+    if
     return
 
 def longest_match(sequence, subsequence):
