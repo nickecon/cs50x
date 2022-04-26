54bac623d161b8aa643ba4485d7b0dfd3499865b
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-26 06:55:07 +0000

    Tue, Apr 26, 2022, 6:55 AM +00:00

--- dna/dna7.py
+++ dna/dna7.py
@@ -53,6 +53,7 @@ def main():
         for i in range(len(STR)):
             row[STR[i]] = int(row[STR[i]])
         #print(row)
+        del row["name"]
         if count == row:
             print("it worked")
             exit(2)
