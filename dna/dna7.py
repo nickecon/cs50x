1c4e0a8a0c76ae394880ca74eb2bbc65f69493eb
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-26 06:59:50 +0000

    Tue, Apr 26, 2022, 6:59 AM +00:00

--- dna/dna7.py
+++ dna/dna7.py
@@ -39,6 +39,7 @@ def main():
     for row in (reader):
         for i in range(len(STR)):
             row[STR[i]] = int(row[STR[i]])
+            continue
 
 
     # TODO: Read DNA sequence file into a variable
