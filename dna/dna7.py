77e1ea0eaa713923f86b2e98613a8dbf5efb024f
nickecon <72775861+nickecon@users.noreply.github.com>
2022-04-26 07:05:03 +0000

    Tue, Apr 26, 2022, 7:05 AM +00:00

--- dna/dna7.py
+++ dna/dna7.py
@@ -55,7 +55,7 @@ def main():
         #print(row)
         del row["name"]
         if count == row:
-            print(row)
+            print(row["name"])
             print("it worked")
             exit(2)
     print("No Match")
