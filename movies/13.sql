8d5aeb2919d5306f713cf0294b82df3194c28920
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 05:31:31 +0000

    Tue, May 10, 2022, 5:31 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -5,4 +5,5 @@ AND title IN (SELECT title FROM movies, stars, people
 WHERE movies.id = stars.movie_id
 AND stars.person_id = people.id
 AND name = "Kevin Bacon"
-AND birth = 1958);
+AND birth = 1958)
+WHERE
