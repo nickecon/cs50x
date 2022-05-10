0ac75b2ce2ed3bdc841e7019479cd9ca53d04abc
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 05:29:16 +0000

    Tue, May 10, 2022, 5:29 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -6,12 +6,3 @@ WHERE movies.id = stars.movie_id
 AND stars.person_id = people.id
 AND name = "Kevin Bacon"
 AND birth = 1958);
-DELETE FROM (SELECT name FROM movies, stars, people
-WHERE movies.id = stars.movie_id
-AND stars.person_id = people.id
-AND title IN (SELECT title FROM movies, stars, people
-WHERE movies.id = stars.movie_id
-AND stars.person_id = people.id
-AND name = "Kevin Bacon"
-AND birth = 1958))
-WHERE name = "Kevin Bacon";
