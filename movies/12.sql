5d216ce8133554b02db94ff2711ff17e934e3854
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 04:52:25 +0000

    Tue, May 10, 2022, 4:52 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,8 +2,7 @@ SELECT title FROM movies, stars, people
 WHERE movies.id = stars.movie_id
 AND stars.person_id = people.id
 AND name = "Helena Bonham Carter"
-IN
-(SELECT title FROM movies, stars, people
+IN movies.id = (SELECT movies.id FROM movies, stars, people
 WHERE movies.id = stars.movie_id
 AND stars.person_id = people.id
 AND name = "Johnny Depp");
