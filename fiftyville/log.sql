6eeeb70848966985a41dda45a863dcd0d9360e1f
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 22:19:37 +0000

    Tue, May 10, 2022, 10:19 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,6 +3,10 @@
 --SELECT * FROM crime_scene_reports;
 --SELECT * FROM bakery_security_logs;
 --SELECT * FROM people;
+
+--SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28
+--UNION
+--SELECT license plate IN people ORDER BY lincese_plate;
+
 SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28
-UNION
-SELECT license_plate FROM people ORDER BY license_plate ASC;
+WHERE license plate IN people;
