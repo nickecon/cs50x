1c1681b10f5fc42c8928654253687bf09c266fa5
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 01:51:12 +0000

    Tue, May 10, 2022, 1:51 AM +00:00

---
+++ movies/10.sql
@@ -0,0 +1,4 @@
+SELECT distinct(name) FROM movies, directors, people
+WHERE movies.id = directors.movie_id
+AND stars.person_id = people.id
+AND rating > 8.9;
--- movies/9.sql
+++ movies/9.sql
@@ -1,3 +1,4 @@
 SELECT distinct(name) FROM movies, stars, people
 WHERE movies.id = stars.movie_id
-AND stars.person_id = people.id and year = 2004 ORDER BY birth;
+AND stars.person_id = people.id
+AND year = 2004 ORDER BY birth;
