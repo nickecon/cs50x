696a23a7384e6ae92537af9d3db641ae181e7f5f
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 01:56:51 +0000

    Tue, May 10, 2022, 1:56 AM +00:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,4 +1,5 @@
-SELECT distinct(name) FROM movies, directors, people, ratings
+SELECT distinct(name) FROM movies, ratings, directors, people
 WHERE movies.id = directors.movie_id
+AND movies.id = ratings.movie_id
 AND directors.person_id = people.id
-AND 
+AND rating < 8.9;
