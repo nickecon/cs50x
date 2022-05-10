ea761b6e543f492eda7bb33f9e5f00e220c24b9d
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 04:34:38 +0000

    Tue, May 10, 2022, 4:34 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -2,7 +2,7 @@ SELECT title FROM movies, stars, people
 WHERE movies.id = stars.movie_id
 AND stars.person_id = people.id
 AND name = "Helena Bonham Carter"
-UNION
+AND
 SELECT title FROM movies, stars, people
 WHERE movies.id = stars.movie_id
 AND stars.person_id = people.id
