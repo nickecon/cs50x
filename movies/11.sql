832d143eaf8144ed6ebcf544a7ba3ca0c98d08ca
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 03:44:17 +0000

    Tue, May 10, 2022, 3:44 AM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,4 +1,4 @@
 SELECT title FROM movies, ratings, people
 WHERE movies.id = ratings.movie_id
 AND movies.id = ratings.movie_id
-AND name = "Chadwick Boseman" ORDER BY rating;
+AND name = "Steve Carell";
