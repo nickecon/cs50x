ea88cf871ea9909f49fcafcf27f71d8af2790942
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 22:35:06 +0000

    Tue, May 10, 2022, 10:35 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,7 +2,7 @@
 --July 28,2021 (7/28/2021) , Humphrey Street
 --SELECT * FROM crime_scene_reports;
 --SELECT * FROM bakery_security_logs;
---SELECT * FROM people;
+--SELECT * FROM people; COUNT = 200
 
 --SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28
 --UNION
@@ -10,4 +10,6 @@
 
 --SELECT * FROM bakery_security_logs JOIN people ON bakery_security_logs.license_plate = people.license_plate;
 
---SELECT * FROM bakery_security_logs JOIN people ON bakery_security_logs.license_plate = people.license_plate WHERE month = 7 and day = 28 ORDER By license_plate;
+--SELECT COUNT(*) FROM bakery_security_logs JOIN people ON bakery_security_logs.license_plate = people.license_plate WHERE month = 7 and day = 28; COUNT = 74
+
+SELECT * FROM bakery_security_logs JOIN people ON bakery_security_logs.license_plate = people.license_plate WHERE month = 7 and day = 28 ORDER BY license_plate;
