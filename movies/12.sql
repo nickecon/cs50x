4a1a28da5b6453e6b70621bef85faa9de7758432
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 05:13:37 +0000

    Tue, May 10, 2022, 5:13 AM +00:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,13 +1,8 @@
-SELECT movies.title
-FROM  movies, people
-JOIN stars
-ON movies.id = stars.movie_id
-AND people.id = stars.person_id
-WHERE people.name = "Helena Bonham Carter"
-AND stars.movie_id IN
-(SELECT movies.title
-FROM movies, people
-JOIN stars
-ON movies.id = stars.movie_id
-AND people.id = stars.person_id
-WHERE people.name = "Johnny Depp");
+SELECT title FROM movies, stars, people
+WHERE movies.id = stars.movie_id
+AND stars.person_id = people.id
+AND name = "Helena Bonham Carter"
+AND title IN (SELECT title FROM movies, stars, people
+WHERE movies.id = stars.movie_id
+AND stars.person_id = people.id
+AND name = "Johnny Depp");
--- movies/9.sql
+++ movies/9.sql
@@ -2,12 +2,3 @@ SELECT distinct(name) FROM movies, stars, people
 WHERE movies.id = stars.movie_id
 AND stars.person_id = people.id
 AND year = 2004 ORDER BY birth;
-
-SELECT title FROM movies, stars, people
-WHERE movies.id = stars.movie_id
-AND stars.person_id = people.id
-AND name = "Helena Bonham Carter"
-AND id = (SELECT id FROM movies, stars, people
-WHERE movies.id = stars.movie_id
-AND stars.person_id = people.id
-AND name = "Johnny Depp");
