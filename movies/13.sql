8239d485d4004636b688ab5a3566672a1c0b7bfb
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 05:40:35 +0000

    Tue, May 10, 2022, 5:40 AM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,4 @@
-SELECT name FROM movies, stars, people
+SELECT name WHERE name != "Kevin Bacon" FROM movies, stars, people
 WHERE movies.id = stars.movie_id
 AND stars.person_id = people.id
 AND title IN (SELECT title FROM movies, stars, people
