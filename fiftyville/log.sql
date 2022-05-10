dae52997e777b2413818dfec9fecff34b446aacd
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-10 21:58:13 +0000

    Tue, May 10, 2022, 9:58 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
--- Keep a log of any SQL queries you execute as you solve the mystery.
-SELECT * FROM crime_scene_reports;
+-- Keep a log of any SQL queries you execute as you solve the mystery
+--July 28,2021 (7/28/2021) , Humphrey Street
+--SELECT * FROM crime_scene_reports;
+SELECT * FROM bakery_security_logs;
