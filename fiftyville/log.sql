adb24ee6734ccf08b0abb404113a1e87bee3ab9a
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-16 18:45:47 +0000

    Mon, May 16, 2022, 6:45 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -30,7 +30,7 @@
 --cross check passengers with the 37 from above, brought back one passport number
 --SELECT * FROM passengers WHERE passport_number = (SELECT passport_number FROM bakery_security_logs JOIN people ON bakery_security_logs.license_plate = people.license_plate WHERE month = 7 and day = 28);
 
---check info of passport number , alice id:809194
+--check info of passport number , alice id:809194 , phone number : (031) 555-9915
 --SELECT * FROM people WHERE passport_number = 1679711307;
 
 --check flights from passport number, flight 16,(seat 3A) & flight 52,(seat 5B)
@@ -48,4 +48,20 @@
 --SELECT * FROM interviews WHERE name = "Alice";
 
 --37
-SELECT COUNT(*) FROM atm_transactions WHERE atm_location = "Humphrey Lane" AND month = 7 AND day = 28; (37)
+--SELECT COUNT(*) FROM atm_transactions WHERE atm_location = "Humphrey Lane" AND month = 7 AND day = 28; (37)
+
+--check bank accounts for alice (bank account number: 37033371)
+--SELECT * FROM bank_accounts WHERE person_id = 809194;
+
+--(770) 555-1861 | 2021 | 7     | 26  | 575      |
+--(420) 555-5821 | 2021 | 7     | 31  | 218      |
+--(496) 555-2096 | 2021 | 7     | 31  | 85       |
+--SELECT * FROM phone_calls WHERE caller = "(031) 555-9915";
+
+-- (789) 555-8833 | 2021 | 7     | 27  | 282      |
+-- (956) 555-1395 | 2021 | 7     | 30  | 433      |
+-- (122) 555-4581 | 2021 | 7     | 31  | 484      |
+-- clues arent leading to much right now
+--SELECT * FROM phone_calls WHERE receiver = "(031) 555-9915";
+
+SELECT * FROM atm_transactions WHERE amount AND month = 7 AND day = 28 AND atm_location = "Humphrey Lane";
