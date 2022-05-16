0e724380be838a217aa56b509de5e9ab8116c0b0
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-16 21:34:55 +0000

    Mon, May 16, 2022, 9:34 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -149,4 +149,7 @@
 --check flights of next day and find earliest flight , flight : 36
 --SELECT * FROM flights WHERE month = 7 AND day = 29 AND origin_airport_id = 8 ORDER BY hour;
 
+Bruce
+Taylor
+were on same flight
 SELECT * FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE flight_id = 36);
