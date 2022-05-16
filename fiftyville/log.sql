594f340f21d927e6b5db5e971c330a81012e33ff
nickecon <72775861+nickecon@users.noreply.github.com>
2022-05-16 20:19:17 +0000

    Mon, May 16, 2022, 8:19 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -109,16 +109,16 @@
 
 --SELECT * FROM interviews WHERE name = "Gloria";
 
---Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
+--10:15am at the Humphrey Street bakery. Interviews, three witnesses, transcripts mentions the bakery.
 --SELECT * FROM crime_scene_reports WHERE street = "Humphrey Street";
 
---Ruth    | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away.
---If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.
+--Ruth | within ten minutes of the theft, saw thief get in car in bakery parking lot and drive away.
+--look for cars that left the parking lot in that time frame(10:15am).
 
---Eugene  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery,
+--Eugene | don't know thief's name, someone I recognized. Earlier this morning, before I arrived at Emma's bakery,
 --I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.
 
 --Raymond | As the thief was leaving the bakery, they called someone who talked to them for less than a minute.
---In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow.
+--I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow(7/28).
 --The thief then asked the person on the other end of the phone to purchase the flight ticket.
 --SELECT * FROM interviews WHERE month = 7 and day = 28;
